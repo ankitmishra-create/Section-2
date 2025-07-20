243278a74e28fced35397230d83e831db1d2a46a
Coditas <Coditas@5836-LAP-0322>
2025-07-20 17:36:43 +0530

    Implemented Switch Satement

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -173,6 +173,43 @@ class ConditionalControlStatement
     
 }
 
+class Switches
+{
+    public static int day;
+    public static void SwitchExample()
+    {
+        day = Convert.ToInt32(Console.ReadLine());
+        switch(day)
+        {
+            case 1:
+                Console.WriteLine("Monday");
+                break;
+            case 2:
+                Console.WriteLine("Tuesday");
+                break;
+            case 3:
+                Console.WriteLine("Wednesday");
+                break;
+            case 4:
+                Console.WriteLine("Thursday");
+                break;
+            case 5:
+                Console.WriteLine("Friday");
+                break;
+            case 6:
+                Console.WriteLine("Saturday");
+                break;
+            case 7:
+                Console.WriteLine("Sunday");
+                break;
+            default:
+                Console.WriteLine("Invalid day");
+                break;
+        }
+
+    }
+}
+
 class Executable
 {
     public static void Main()
@@ -183,6 +220,7 @@ class Executable
         NearestThousand.RoundToNearestThousand(123456.789);
         SecondsToMinutes.SecondToMinute(120);
         ConditionalControlStatement.Heights(75);
+        Switches.SwitchExample();
 
     }
 }
